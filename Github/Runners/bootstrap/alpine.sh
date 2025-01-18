1c0c32a48d8189a0bdc0db360d80c0d336b52830
Azathothas <AjamX101@gmail.com>
2025-01-18 10:02:19 +0545

    more

---
+++ Github/Runners/bootstrap/alpine.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+##DO NOT RUN DIRECTLY
+##Self: bash <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge/devscripts/refs/heads/main/Github/Runners/bootstrap/alpine.sh")
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+set -x
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Bootstrap
+ pushd "$(mktemp -d)" >/dev/null 2>&1
+  docker stop "alpine-base" 2>/dev/null ; docker rm "alpine-base" 2>/dev/null
+  docker run --name "alpine-base" --privileged "alpine:edge" sh -l -c '
+  #Bootstrap
+   apk update --no-interactive
+   apk upgrade --no-interactive
+   apk add bash binutils curl fakeroot musl-locales-lang wget --latest --upgrade --no-interactive
+   #apk add bash alsa-utils alsa-utils-doc alsa-lib alsaconf alsa-ucm-conf pulseaudio pulseaudio-alsa --latest --upgrade --no-interactive
+  #Fix & Patches
+   chmod 755 "/bin/bbsuid" 2>/dev/null
+   echo -e "nameserver 8.8.8.8\nnameserver 2620:0:ccc::2" | tee "/etc/resolv.conf"
+   echo -e "nameserver 1.1.1.1\nnameserver 2606:4700:4700::1111" | tee -a "/etc/resolv.conf"
+   mkdir -pv "/etc/apk"
+   echo "https://dl-cdn.alpinelinux.org/alpine/edge/main" | tee "/etc/apk/repositories"
+   echo "https://dl-cdn.alpinelinux.org/alpine/edge/community" | tee -a "/etc/apk/repositories"
+   echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" | tee -a "/etc/apk/repositories"
+   echo "LANG=en_US.UTF-8" | tee "/etc/locale.conf"
+   echo "LANG=en_US.UTF-8" | tee -a "/etc/locale.conf"
+   echo "LANGUAGE=en_US:en" | tee -a "/etc/locale.conf"
+   echo "LC_ALL=en_US.UTF-8" | tee -a "/etc/locale.conf"
+   locale-gen
+   locale-gen "en_US.UTF-8"
+  #Cleanup
+   rm -rfv "/"{tmp,proc,sys,dev,run}
+   mkdir -pv "/"{tmp,proc,sys,dev,run/media,mnt,media,home}
+   rm -fv /etc/{host.conf,hosts,passwd,group,nsswitch.conf}
+   touch /etc/{host.conf,hosts,passwd,group,nsswitch.conf}
+   apk info -L
+   rm -rfv "/var/cache/apk/"*'
+##Export   
+  docker export "$(docker ps -aqf 'name=alpine-base')" --output "rootfs.tar"
+  if [[ -f "./rootfs.tar" ]] && [[ $(stat -c%s "./rootfs.tar") -gt 10000 ]]; then
+    rsync -achLv --mkpath "./rootfs.tar" "/tmp/rootfs.tar"
+  else
+    echo "\n[-] FATAL: Failed to export ROOTFS\n"
+   exit 1
+  fi
+popd "$(mktemp -d)" >/dev/null 2>&1
+#-------------------------------------------------------#
+
+
+#-------------------------------------------------------#
+##Push
+#ENV
+D_ID="$(docker ps -aqf 'name=alpine-base' | tr -d '[:space:]')"
+D_TAG="v$(date +'%Y.%m.%d' | tr -d '[:space:]')"
+export D_ID D_TAG
+#Tags
+docker commit "${D_ID}" "pkgforge/alpine-base:latest"
+docker commit "${D_ID}" "ghcr.io/pkgforge/devscripts/alpine-base:latest"
+docker commit "${D_ID}" "pkgforge/alpine-base:${D_TAG}"
+docker commit "${D_ID}" "ghcr.io/pkgforge/devscripts/alpine-base:${D_TAG}"
+docker commit "${D_ID}" "pkgforge/alpine-base:$(uname -m)"
+docker commit "${D_ID}" "ghcr.io/pkgforge/devscripts/alpine-base:$(uname -m)"
+#Push
+docker push "pkgforge/alpine-base:latest"
+docker push "ghcr.io/pkgforge/devscripts/alpine-base:latest"
+docker push "pkgforge/alpine-base:${D_TAG}"
+docker push "ghcr.io/pkgforge/devscripts/alpine-base:${D_TAG}"
+docker push "pkgforge/alpine-base:$(uname -m)"
+docker push "ghcr.io/pkgforge/devscripts/alpine-base:$(uname -m)"
+#-------------------------------------------------------#
---
+++ Github/Runners/bootstrap/ubuntu.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+#
+##DO NOT RUN DIRECTLY
+##Self: bash <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge/devscripts/refs/heads/main/Github/Runners/bootstrap/ubuntu.sh")
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+## https://github.com/phusion/baseimage-docker
+# https://hub.docker.com/r/phusion/baseimage/tags
+set -x
+#-------------------------------------------------------#
+
+#-------------------------------------------------------#
+##Bootstrap
+ pushd "$(mktemp -d)" >/dev/null 2>&1
+  docker stop "ubuntu-base" 2>/dev/null ; docker rm "ubuntu-base" 2>/dev/null
+  docker run --name "ubuntu-base" --privileged "phusion/baseimage:noble-1.0.0" sh -l -c '
+  #Bootstrap
+   #echo -e "nameserver 8.8.8.8\nnameserver 2620:0:ccc::2" | tee "/etc/resolv.conf"
+   #echo -e "nameserver 1.1.1.1\nnameserver 2606:4700:4700::1111" | tee -a "/etc/resolv.conf"
+   unlink "/var/lib/dbus/machine-id" 2>/dev/null
+   unlink "/etc/machine-id" 2>/dev/null
+   rm -rvf "/etc/machine-id"
+   systemd-machine-id-setup --print 2>/dev/null | tee "/var/lib/dbus/machine-id"
+   ln --symbolic --force --relative "/var/lib/dbus/machine-id" "/etc/machine-id"
+   echo "LANG=en_US.UTF-8" | tee "/etc/locale.conf"
+   echo "LANG=en_US.UTF-8" | tee -a "/etc/locale.conf"
+   echo "LANGUAGE=en_US:en" | tee -a "/etc/locale.conf"
+   echo "LC_ALL=en_US.UTF-8" | tee -a "/etc/locale.conf"
+   echo "en_US.UTF-8 UTF-8" | tee -a "/etc/locale.gen"
+   echo "LC_ALL=en_US.UTF-8" | tee -a "/etc/environment"
+   chown -R _apt:root /var/cache/apt/archives/partial/
+   dpkg-statoverride --remove /usr/bin/crontab
+   ubuntu_FRONTEND="noninteractive" apt update -y
+   ubuntu_FRONTEND="noninteractive" apt install bash binutils coreutils curl fakeroot git locales sudo wget -y --no-install-recommends --ignore-missing
+   locale-gen "en_US.UTF-8"
+   echo "debconf debconf/frontend select Noninteractive" | debconf-set-selections
+   apt purge locales perl -y ; apt autoremove -y ; apt autoclean -y
+   apt list --installed
+   apt clean -y
+   find "/boot" -mindepth 1 -delete 2>/dev/null
+   find "/dev" -mindepth 1 -delete 2>/dev/null
+   find "/proc" -mindepth 1 -delete 2>/dev/null
+   find "/run" -mindepth 1 -delete 2>/dev/null
+   find "/sys" -mindepth 1 -delete 2>/dev/null
+   find "/tmp" -mindepth 1 -delete 2>/dev/null
+   find "/usr/include" -mindepth 1 -delete 2>/dev/null
+   find "/usr/lib" -type f -name "*.a" -print -exec rm -rfv "{}" 2>/dev/null \; 2>/dev/null
+   find "/usr/lib32" -type f -name "*.a" -print -exec rm -rfv "{}" 2>/dev/null \; 2>/dev/null
+   find "/usr/share/locale" -mindepth 1 -maxdepth 1 ! -regex '\''.*/\(locale.alias\|en\|en_US\)$'\'' -exec rm -rfv "{}" + 2>/dev/null
+   find "/usr/share/doc" -mindepth 1 -delete 2>/dev/null
+   find "/usr/share/gtk-doc" -mindepth 1 -delete 2>/dev/null
+   find "/usr/share/help" -mindepth 1 -delete 2>/dev/null
+   find "/usr/share/info" -mindepth 1 -delete 2>/dev/null
+   find "/usr/share/man" -mindepth 1 -delete 2>/dev/null
+   find "/" -type d -name "__pycache__" -exec rm -rfv "{}" \; 2>/dev/null
+   find "/" -type f -name "*.pacnew" -exec rm -rfv "{}" \; 2>/dev/null
+   find "/" -type f -name "*.pacsave" -exec rm -rfv "{}" \; 2>/dev/null
+   find "/var/log" -type f -name "*.log" -exec rm -rfv "{}" \; 2>/dev/null
+   rm -rfv "/var/lib/apt/lists/"*
+   rm -rfv "/var/cache/apt/"*
+   rm -rfv "/"{tmp,proc,sys,dev,run}
+   mkdir -pv "/"{tmp,proc,sys,dev,run/media,mnt,media,home}
+   rm -fv /etc/{host.conf,hosts,passwd,group,nsswitch.conf}
+   touch /etc/{host.conf,hosts,passwd,group,nsswitch.conf}'
+##Export   
+  docker export "$(docker ps -aqf 'name=ubuntu-base')" --output "rootfs.tar"
+  if [[ -f "./rootfs.tar" ]] && [[ $(stat -c%s "./rootfs.tar") -gt 10000 ]]; then
+    rsync -achLv --mkpath "./rootfs.tar" "/tmp/rootfs.tar"
+  else
+    echo "\n[-] FATAL: Failed to export ROOTFS\n"
+   exit 1
+  fi
+popd "$(mktemp -d)" >/dev/null 2>&1
+#-------------------------------------------------------#
+
+
+#-------------------------------------------------------#
+##Push
+#ENV
+D_ID="$(docker ps -aqf 'name=ubuntu-base' | tr -d '[:space:]')"
+D_TAG="v$(date +'%Y.%m.%d' | tr -d '[:space:]')"
+export D_ID D_TAG
+#Tags
+docker commit "${D_ID}" "pkgforge/ubuntu-base:latest"
+docker commit "${D_ID}" "ghcr.io/pkgforge/devscripts/ubuntu-base:latest"
+docker commit "${D_ID}" "pkgforge/ubuntu-base:${D_TAG}"
+docker commit "${D_ID}" "ghcr.io/pkgforge/devscripts/ubuntu-base:${D_TAG}"
+docker commit "${D_ID}" "pkgforge/ubuntu-base:$(uname -m)"
+docker commit "${D_ID}" "ghcr.io/pkgforge/devscripts/ubuntu-base:$(uname -m)"
+#Push
+docker push "pkgforge/ubuntu-base:latest"
+docker push "ghcr.io/pkgforge/devscripts/ubuntu-base:latest"
+docker push "pkgforge/ubuntu-base:${D_TAG}"
+docker push "ghcr.io/pkgforge/devscripts/ubuntu-base:${D_TAG}"
+docker push "pkgforge/ubuntu-base:$(uname -m)"
+docker push "ghcr.io/pkgforge/devscripts/ubuntu-base:$(uname -m)"
+#-------------------------------------------------------#
