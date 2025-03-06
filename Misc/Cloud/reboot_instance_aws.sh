97d9735845ed446cd9af6459d5e74f8fdd233ccd
Azathothas <AjamX101@gmail.com>
2025-03-06 11:24:25 +0545

    +

---
+++ Misc/Cloud/reboot_instance_aws.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+##Requirements: aws-wire-lengths bash coreutils curl findutils jq moreutils
+
+##Usage:
+# bash <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge/devscripts/refs/heads/main/Misc/Cloud/eboot_instance_aws.sh")
+
+#----------------------------------------------------------------------------#
+##Sanity Checks
+   SYSTMP="$(dirname $(mktemp -u))" && export SYSTMP="${SYSTMP}"
+   USER_AGENT="$(curl -qfsSL 'https://raw.githubusercontent.com/pkgforge/devscripts/refs/heads/main/Misc/User-Agents/ua_firefox_macos_latest.txt')" && export USER_AGENT="${USER_AGENT}"
+ #Curl
+   if ! command -v curl &> /dev/null; then
+     echo -e "\n[-] FATAL: Install Curl (https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/curl)\n"
+   exit 1
+   fi
+ #Get Bin  
+   if command -v sudo &> /dev/null && sudo -n true 2>/dev/null; then
+     sudo curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/aws-wire-lengths" -o "/usr/local/bin/aws-wire-lengths" && sudo chmod +x "/usr/local/bin/aws-wire-lengths"
+     sudo curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/ipinfo" -o "/usr/local/bin/ipinfo" && sudo chmod +x "/usr/local/bin/ipinfo"
+     sudo curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/jq" -o "/usr/local/bin/jq" && sudo chmod +x "/usr/local/bin/jq"
+   else
+     curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/aws-wire-lengths" -o "${SYSTMP}/aws-wire-lengths" && chmod +x "${SYSTMP}/aws-wire-lengths"
+     curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/ipinfo" -o "${SYSTMP}/ipinfo" && chmod +x "${SYSTMP}/ipinfo"
+     curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/jq" -o "${SYSTMP}/jq" && chmod +x "${SYSTMP}/jq"
+     export PATH="${PATH}:${SYSTMP}"
+   fi
+ #AWS_ACCESS_KEY_ID
+   if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "${AWS_ACCESS_KEY_ID+x}" ]; then
+     echo -e "\n[-] AWS_ACCESS_KEY_ID isn't Exported\n"
+    exit 1
+   fi
+ #AWS_ACCESS_KEY_ID
+   if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "${AWS_SECRET_ACCESS_KEY+x}" ]; then
+     echo -e "\n[-] AWS_SECRET_ACCESS_KEY isn't Exported\n"
+    exit 1
+   fi
+ #AWS_REGION
+   if [ -z "$AWS_REGION" ] || [ -z "${AWS_REGION+x}" ]; then
+     echo -e "\n[-] AWS_REGION isn't Exported\n"
+    exit 1
+   fi
+ #EC2_INSTANCE_NAME
+   if [ -z "$EC2_INSTANCE_NAME" ] || [ -z "${EC2_INSTANCE_NAME+x}" ]; then
+     echo -e "\n[-] EC2_INSTANCE_NAME isn't Exported\n"
+    exit 1
+   fi
+ #EC2_INSTANCE_ID
+   if [ -z "$EC2_INSTANCE_ID" ] || [ -z "${EC2_INSTANCE_ID+x}" ]; then
+     echo -e "\n[-] EC2_INSTANCE_ID isn't Exported\n"
+    exit 1
+   fi
+#----------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------#
+#List
+ aws-wire-lengths --region-ec2 "${AWS_REGION}" instance list
+#Get IP
+ EC2_IP_OLD="$(aws-wire-lengths --region-ec2 "${AWS_REGION}" instance ip "${EC2_INSTANCE_NAME}" | tr -d '[:space:]')" && export EC2_IP_OLD="${EC2_IP_OLD}"
+ echo -e "\n[+] IPv4 Address: ${EC2_IP_OLD}\n"
+ #curl -A "${USER_AGENT}" -H "Accept: application/json" -qfsSL "https://ipinfo.io/${EC2_IP_OLD}/json" | jq . ; echo
+ ipinfo "${EC2_IP_OLD}" --pretty 2>/dev/null
+#Start/Stop: https://web.archive.org/web/2/https://devopslearning.medium.com/differences-between-aws-ec2-instance-start-stop-and-restart-operation-35f98f9e1065
+ echo -e "\n[+] Stopping... ${EC2_INSTANCE_NAME}"
+ aws-wire-lengths --region-ec2 "${AWS_REGION}" instance stop "${EC2_INSTANCE_NAME}" >/dev/null 2>&1
+ echo -e "[+] Starting... ${EC2_INSTANCE_NAME}\n"
+ aws-wire-lengths --region-ec2 "${AWS_REGION}" instance start "${EC2_INSTANCE_NAME}" >/dev/null 2>&1
+#List again
+ aws-wire-lengths --region-ec2 "${AWS_REGION}" instance list
+#Recheck IP
+ EC2_IP_NEW="$(aws-wire-lengths --region-ec2 "${AWS_REGION}" instance ip "${EC2_INSTANCE_NAME}" | tr -d '[:space:]')" && export EC2_IP_NEW="${EC2_IP_NEW}"
+ echo -e "\n[+] IPv4 Address: ${EC2_IP_NEW}\n"
+ #curl -A "${USER_AGENT}" -H "Accept: application/json" -qfsSL "https://ipinfo.io/${EC2_IP_NEW}/json" | jq . ; echo
+ ipinfo "${EC2_IP_NEW}" --pretty 2>/dev/null
+#END
+unset EC2_IP_OLD EC2_IP_NEW
+#EOF
+#----------------------------------------------------------------------------#
---
+++ Misc/Cloud/reboot_instance_cntb.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+##Requirements: cntb bash coreutils curl findutils jq moreutils
+
+##Config: https://my.contabo.com/api/details
+# export CONTABO_CLIENT_ID='id'
+# export CONTABO_CLIENT_SECRET='secret'
+# export CONTABO_API_USERNAME='email'
+# export CONTABO_API_PASSWORD='api_password_NOT_account_password'
+# export INSTANCE_HOST_NUMBER="Host Number from: https://my.contabo.com/vps/0#"
+
+##Usage:
+# bash <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge/devscripts/refs/heads/main/Misc/Cloud/reboot_instance_cntb.sh")
+
+#----------------------------------------------------------------------------#
+##Sanity Checks
+   SYSTMP="$(dirname $(mktemp -u))" && export SYSTMP="${SYSTMP}"
+   USER_AGENT="$(curl -qfsSL 'https://raw.githubusercontent.com/pkgforge/devscripts/refs/heads/main/Misc/User-Agents/ua_firefox_macos_latest.txt')" && export USER_AGENT="${USER_AGENT}"
+ #Curl
+   if ! command -v curl &> /dev/null; then
+     echo -e "\n[-] FATAL: Install Curl (https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/curl)\n"
+   exit 1
+   fi
+ #Get Bin  
+   if command -v sudo &> /dev/null && sudo -n true 2>/dev/null; then
+     sudo curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/cntb" -o "/usr/local/bin/cntb" && sudo chmod +x "/usr/local/bin/cntb"
+     sudo curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/ipinfo" -o "/usr/local/bin/ipinfo" && sudo chmod +x "/usr/local/bin/ipinfo"
+     sudo curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/jq" -o "/usr/local/bin/jq" && sudo chmod +x "/usr/local/bin/jq"
+   else
+     curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/cntb" -o "${SYSTMP}/cntb" && chmod +x "${SYSTMP}/cntb"
+     curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/ipinfo" -o "${SYSTMP}/ipinfo" && chmod +x "${SYSTMP}/ipinfo"
+     curl -A "${USER_AGENT}" -qfsSL "https://bin.pkgforge.dev/$(uname -m)-$(uname -s)/jq" -o "${SYSTMP}/jq" && chmod +x "${SYSTMP}/jq"
+     export PATH="${PATH}:${SYSTMP}"
+   fi
+ #CONTABO_CLIENT_ID
+   if [ -z "$CONTABO_CLIENT_ID" ] || [ -z "${CONTABO_CLIENT_ID+x}" ]; then
+     echo -e "\n[-] CONTABO_CLIENT_ID isn't Exported: https://my.contabo.com/api/details\n"
+    exit 1
+   fi
+ #CONTABO_CLIENT_SECRET
+   if [ -z "$CONTABO_CLIENT_SECRET" ] || [ -z "${CONTABO_CLIENT_SECRET+x}" ]; then
+     echo -e "\n[-] CONTABO_CLIENT_SECRET isn't Exported: https://my.contabo.com/api/details\n"
+    exit 1
+   fi
+ #CONTABO_API_USERNAME
+   if [ -z "$CONTABO_API_USERNAME" ] || [ -z "${CONTABO_API_USERNAME+x}" ]; then
+     echo -e "\n[-] CONTABO_API_USERNAME isn't Exported: https://my.contabo.com/api/details\n"
+    exit 1
+   fi
+ #CONTABO_API_PASSWORD
+   if [ -z "$CONTABO_API_PASSWORD" ] || [ -z "${CONTABO_API_PASSWORD+x}" ]; then
+     echo -e "\n[-] CONTABO_API_PASSWORD isn't Exported: https://my.contabo.com/api/details\n"
+    exit 1
+   fi
+ #INSTANCE_HOST_NUMBER
+   if [ -z "$INSTANCE_HOST_NUMBER" ] || [ -z "${INSTANCE_HOST_NUMBER+x}" ]; then
+     echo -e "\n[-] INSTANCE_HOST_NUMBER isn't Exported: https://my.contabo.com/api/details\n"
+    exit 1
+   fi
+#----------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------#
+#Set config [ "$HOME/.cntb.yaml" || "/etc/cntb/.cntb.yaml" ]
+ #cntb config set-credentials --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info"
+ rm -rf "$HOME/.cntb.yaml" 2>/dev/null ; sudo rm -rf "/etc/cntb/.cntb.yaml" 2>/dev/null
+#List
+ cntb get instances --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info"
+#Get Instance_ID
+ CONTABO_INSTANCE_ID="$(cntb get instances --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info" --output="json" | jq --arg HN "${INSTANCE_HOST_NUMBER}" '.[] | select(.vHostNumber == ($HN | tonumber))' | jq -r '.instanceId')" && export CONTABO_INSTANCE_ID="${CONTABO_INSTANCE_ID}"
+ CONTABO_INSTANCE_IP="$(cntb get instances --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info" --output="json" | jq --arg HN "${INSTANCE_HOST_NUMBER}" '.[] | select(.vHostNumber == ($HN | tonumber))' | jq -r '.ipv4')" && export CONTABO_INSTANCE_IP="${CONTABO_INSTANCE_IP}"
+#Reboot
+ echo -e "\n[+] Stopping... ${CONTABO_INSTANCE_ID} (${INSTANCE_HOST_NUMBER})"
+ cntb stop instance "${CONTABO_INSTANCE_ID}" --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info" && sleep 10
+ cntb get instance "${CONTABO_INSTANCE_ID}" --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info"
+ echo -e "\n[+] Starting... ${CONTABO_INSTANCE_ID} (${INSTANCE_HOST_NUMBER})"
+ cntb start instance "${CONTABO_INSTANCE_ID}" --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info" && sleep 10
+ cntb get instance "${CONTABO_INSTANCE_ID}" --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info"
+ cntb get instance "${CONTABO_INSTANCE_ID}" --oauth2-clientid="$(echo "${CONTABO_CLIENT_ID}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-client-secret="$(echo "${CONTABO_CLIENT_SECRET}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-user="$(echo "${CONTABO_API_USERNAME}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --oauth2-password="$(echo "${CONTABO_API_PASSWORD}" | sed -e 's/"//g' -e "s/'//g" | tr -d '[:space:]')" --debug="info" --output="yaml"
+#Get IP Info
+ #curl -A "${USER_AGENT}" -H "Accept: application/json" -qfsSL "https://ipinfo.io/${CONTABO_INSTANCE_IP}/json" | jq . ; echo
+ ipinfo "${CONTABO_INSTANCE_IP}" --pretty 2>/dev/null
+#END
+unset CONTABO_INSTANCE_ID
+rm -rf "$HOME/.cntb.yaml" 2>/dev/null ; sudo rm -rf "/etc/cntb/.cntb.yaml" 2>/dev/null
+#EOF
+#----------------------------------------------------------------------------#
